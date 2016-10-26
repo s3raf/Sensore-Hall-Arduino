d97d3d0677af4c80fbf0139f93dd4da3b2174797
Mauro Serafin <prv972@gmail.com>
2016-10-26 21:17:57 +0200

    Initial Commit

---
+++ HallSensor/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HallSensor
+//
+//  Created by Mauro on 26/10/16.
+//  Copyright © 2016 SiR4U. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
